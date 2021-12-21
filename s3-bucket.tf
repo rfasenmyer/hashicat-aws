122176bc5b2db9ee793554d067043cafc515c49c
Ryan Fasenmyer <github@rfdev.com>
2021-12-21 19:14:17 +0000

    addes s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   version = "2.2.0"
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "hairball"
   acl    = "private"
   bucket_prefix = var.prefix
 
